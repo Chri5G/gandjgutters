b4596471a6bd4121338b6045e6fcf4f75c4c7b23
noveon-cguillermo <cguillermo@noveon.co>
2025-03-19 21:55:10 -0500

    added gutters route component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Intro';
-import Intro from './components/Gutters/Intro';
+import Gutters from './components/Gutters/Gutters';
 import Residential from './components/Gutters/Residential';
 import Commercial from './components/Gutters/Commercial';
-import Footer from './components/Footer';
+import Roofing from './components/Roofing/Roofing';
+import Metal from './components/Roofing/Metal';
 
-// import Contact from './components/Contact';
-// import NotFound from './components/NotFound';
+import Footer from './components/Footer';
+import NotFound from './components/NotFound';
 
 
 function App() {
@@ -17,9 +18,13 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path='/gutters' element={<Intro />} >
+        <Route path='/gutters' element={<Gutters />} >
           <Route path='/gutters/residential' element={<Residential />} />
           <Route path='/gutters/commercial' element={<Commercial />} />
+        </Route>
+        <Route path='/roofing' element={<Roofing />} >
+          <Route path='/roofing/residential' element={<Metal />} />
+          <Route path='/roofing/commercial' element={<Commercial />} />
         </Route>
         {/* Catch-all route for 404 page */}
         {/* <Route path="*" element={<NotFound />} /> */}
